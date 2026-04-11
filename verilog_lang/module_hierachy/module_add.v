e798ebf96d7d45ad6b13de35d6a0cda255816b5e
Megalan <megalan4444@gmail.com>
2026-04-11 14:35:56 +0530

    Create module_add.v

---
+++ verilog_lang/module_hierachy/module_add.v
@@ -0,0 +1,10 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire c1;
+    add16 m1(.a(a[15:0]),.b(b[15:0]),.cin(1'b0),.cout(c1),.sum(sum[15:0]));
+    add16 m2(.a(a[31:16]),.b(b[31:16]),.cin(c1),.cout(),.sum(sum[31:16]));
+
+endmodule
