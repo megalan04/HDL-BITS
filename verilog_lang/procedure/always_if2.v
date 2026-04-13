904aa986c7ffddabed4caeb5bd8e0c22e512cfec
Megalan <megalan4444@gmail.com>
2026-04-13 20:59:23 +0530

    Update always_if2.v

--- verilog_lang/procedure/always_if2.v
+++ verilog_lang/procedure/always_if2.v
@@ -1,18 +1,18 @@
-module top_module(
-    input a,
-    input b,
-    input sel_b1,
-    input sel_b2,
-    output wire out_assign,
-    output reg out_always   ); 
-    assign out_assign=(sel_b1&sel_b2)?b:a;
-    always@(*)begin
-        if(sel_b1&sel_b2)
-            out_always=b;
-        else
-            out_always=a;
-        
+module top_module (
+    input      cpu_overheated,
+    output reg shut_off_computer,
+    input      arrived,
+    input      gas_tank_empty,
+    output reg keep_driving  ); //
+
+    always @(*) begin
+        if (cpu_overheated)
+           shut_off_computer = 1;
     end
-    
+
+    always @(*) begin
+        if (~arrived)
+           keep_driving = ~gas_tank_empty;
+    end
 
 endmodule
