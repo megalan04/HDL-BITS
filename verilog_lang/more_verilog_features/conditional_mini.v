2369db42b51bd3b388afefcc529f907836a3d23f
Megalan <megalan4444@gmail.com>
2026-04-13 12:17:02 +0530

    Create conditional_mini.v

---
+++ verilog_lang/more_verilog_features/conditional_mini.v
@@ -0,0 +1,9 @@
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);
+    wire [7:0]e,f;
+    assign e=(a<b)?a:b;
+    assign f=(e<c)?e:c;
+    assign min=(f<d)?f:d;
+
+endmodule
