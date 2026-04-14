ac3a51a8a9e41e354b4451ad52b15f8f4445e649
Megalan <megalan4444@gmail.com>
2026-04-14 12:28:23 +0530

    Create testbench1.v

---
+++ VERIFICATION:WRITING TESTBENCH/testbench1.v
@@ -0,0 +1,15 @@
+module top_module ( output reg A, output reg B );
+    initial begin
+       A=0;
+       B=0;
+        
+       #10 A=1;
+       #10 A=0;
+    end
+     initial begin   
+       #15 B=1;
+       #25 B=0; 
+
+    end
+
+endmodule
