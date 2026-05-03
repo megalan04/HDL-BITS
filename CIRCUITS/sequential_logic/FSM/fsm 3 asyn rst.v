752a51331120bd5fa3cfd9f2d3bf5388d296309c
Megalan <megalan4444@gmail.com>
2026-05-03 15:07:53 +0530

    Create fsm 3 asyn rst.v

---
+++ CIRCUITS/sequential_logic/FSM/fsm 3 asyn rst.v
@@ -0,0 +1,30 @@
+module top_module(
+    input clk,
+    input in,
+    input areset,
+    output out); 
+    parameter A=2'b00,
+              B=2'b01,
+              C=2'b10,
+              D=2'b11;
+    reg[1:0]state,next_state;
+    always@(posedge clk or posedge areset)begin
+        if(areset)begin
+            state<=A;
+        end
+        else
+            state<=next_state;
+    end
+    always@(*)begin
+        case(state)
+            A:next_state=in?B:A;
+            B:next_state=in?B:C;
+            C:next_state=in?D:A;
+            D:next_state=in?B:C;
+            default:next_state=A;
+        endcase
+    end
+    assign out=state==D;
+            
+
+endmodule
